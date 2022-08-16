f2332affb0ac6a2837cc0ebaf46c69003efe9bc1
Martim Correia <correia_martim@hotmail.com>
2022-08-16 16:48:42 +0100

    front end not done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles, except: :index
+  root to: 'articles#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts 'Creating 10 articles...'
+
+10.times do
+  article = Article.new(
+    title: Faker::Esport.game,
+    content: Faker::Esport.event
+  )
+  article.save!
+end
+
+puts 'Done'
